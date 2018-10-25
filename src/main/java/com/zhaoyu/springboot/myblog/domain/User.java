557384fa5e144673059e774f96629ac610771c6e
15250 <756834111@qq.com>
2018-10-25 17:52:36 +0800

    2018-10-25

--- src/main/java/com/zhaoyu/springboot/myblog/domain/User.java
+++ src/main/java/com/zhaoyu/springboot/myblog/domain/User.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class User implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;//id 主键
 
     private String username;//账号
 
@@ -19,9 +19,22 @@ public class User implements Serializable{
 
     private String name;//用户名
 
+    private String email;//邮箱
+
     private String sex;//性别
 
     private String realName;//真实姓名
 
     private Date birthday;//生日
+
+    private String localtion;//区域
+
+    private String trade;//行业
+
+    private String position;//职位
+
+    private String avatar;//头像
+
+    protected User() { // JPA 的规范要求无参构造函数；设为 protected 防止直接使用
+    }
 }
